921e3792117ec6d602c132b57683b489f81754c7
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2023-08-07 12:50:54 +0200

    define the enum for category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -3,15 +3,20 @@ import 'package:uuid/uuid.dart';
 // Uuid libreria esterna per creare degli ID
 const uuid = Uuid();
 
+// un insieme ben definito di costanti
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
